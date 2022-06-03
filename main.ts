a4dfaa6cbd14960a46d5fab935c2eeaf37770801
Jiaming Zhang <jzhang@brex.com>
2022-06-03 10:27:18 -0700

    Remove unused Modal

--- main.ts
+++ main.ts
@@ -223,22 +223,6 @@ interface GithubFileContent {
 }
 
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
-}
-
 class ObsidianGithubSettingTab extends PluginSettingTab {
 	plugin: ObsidianGithub;
 
